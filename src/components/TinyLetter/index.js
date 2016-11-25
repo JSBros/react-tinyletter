bbff6247bea91e8a1e0ee1ac5423411c23c06e8c
Garet McKinley <self@mediachicken.com>
2016-11-24 20:02:21 -0500

    chore: added docs via esdoc

--- src/components/TinyLetter/index.js
+++ src/components/TinyLetter/index.js
@@ -1,5 +1,28 @@
 import React from 'react'
 
+/**
+ * The TinyLetter component acts as a wrapper
+ * for the subscription form. It does not automatically
+ * populate itself with input fields. Because of this,
+ * To do a minimum implementation, you will need to do:
+ *
+ * ```jsx
+ * <TinyLetter list="YourTinyLetterUsername">
+ *   <input type="email"/>
+ *   <input type="submit" value="Subscribe"/>
+ * </TinyLetter>
+ * ```
+ * 
+ * You need not worry about assigning the `name` or `id`
+ * properties on form inputs, they will automatically get
+ * added by the `TinyLetter` component. **However**, make
+ * sure that you use the correct `type` on your input fields.
+ * The `type` is how the `TinyLetter` component is able to
+ * determine which `input` is which.
+ *
+ * @param {object} props - jsx attributes
+ * @return {ReactElement} - form wrapper markup
+ */
 function TinyLetter(props) {
   function subscribe() {
     window.open(`https://tinyletter.com/${props.list}`,
